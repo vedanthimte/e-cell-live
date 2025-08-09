04020cc6428b8e094efcb58204c30db8ed5778e9
anand-katariya <akxiianandkatariya@gmail.com>
2025-08-09 16:04:08 +0530

    //

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import Logo from "../public/E-CELL.png";
 
 export const metadata: Metadata = {
   title: "E-CELL PRMIT&R",
   description: "E-CELL PRMIT&R",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>
