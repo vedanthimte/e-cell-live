127eedf46af3c360dd306b062452c9b2fb5a08c7
anand-katariya <akxiianandkatariya@gmail.com>
2025-08-09 13:07:46 +0530

    aa

--- src/app/registerform/page.tsx
+++ src/app/registerform/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Register() {
   return (
-    <section className="w-full min-h-screen bg-[#0E001B] flex justify-center items-center p-4">
+    <section className="w-full min-h-screen bg-[#0E001B] flex justify-center items-center pt-20 p-4">
       <div className="bg-[#1C0233] p-6 rounded-xl shadow-lg w-full max-w-3xl">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSegyCTCrP-V_bPBjkD5cGMeost4XZ9BnV0aFLbHXV7ntWYx2Q/viewform?embedded=true"
